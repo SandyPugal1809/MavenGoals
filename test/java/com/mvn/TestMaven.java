b635cdcd1f5ae6357d670ab686da14c99b0bba9a
sandy1809 <santhoshsakesha1809@gmail.com>
2023-06-24 14:57:37 +0530

    mavenGoals

---
+++ test/java/com/mvn/TestMaven.java
@@ -0,0 +1,19 @@
+package com.mvn;
+
+import org.junit.Test;
+
+public class TestMaven {
+	
+	
+	@Test
+	public void testApple() {
+	
+		System.out.println("test.........");
+	}
+
+	@Test
+	public void testOrange() {
+		
+		System.out.println("test.........1");
+	}
+}
---
+++ test/java/org/sam/WiproTest.java
@@ -0,0 +1,26 @@
+package org.sam;
+
+import org.junit.Test;
+
+public class WiproTest {
+	
+	@Test
+	public void alpha() {
+	System.out.println("test....111");
+
+	}
+	
+	@Test
+	public void beta() {
+		System.out.println("test....222");
+
+		}
+	
+	@Test
+	public void gamma() {
+		System.out.println("orange");
+
+		}
+
+
+}
